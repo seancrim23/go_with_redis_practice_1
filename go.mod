a55fae3b9aecb0966285c4eb271a1b10c4c5859b
sean <sean.g.crim@gmail.com>
2025-03-24 18:34:49 -0400

    followed first video for basic setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_with_redis_practice_1
+
+go 1.20
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen and server: ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
